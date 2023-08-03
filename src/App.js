e1ae6688091d4ef804bcbafe90775f327075142c
kauan.meira <kauangabriel.meira123@gmail.com>
2023-08-03 15:32:22 -0300

    implementação da view Login

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
 import { render } from "@testing-library/react";
 import React from "react";
+import 'bootswatch/dist/flatly/bootstrap.css'
+import Login from './views/login'
+import './custom.css'
 
-
-    class App extends React.Component{
-state = {
-  numero1: '',
-  numero2: '',
-  resultado: ''
-}
-
-      render(){
+class App extends React.Component {
+    render() {
         return (
-          <div>
-            <label>
-              Primeiro Numero:
-            </label>
-            <input type ="text" value = {this.state.numero1} 
-            onChange ={(e) => this.setState({numero1: e.target.value})}/>
-             <label>
-              Primeiro Numero:
-            </label>
-             <input type ="text" value = {this.state.numero2} 
-            onChange ={(e) => this.setState({numero2: e.target.value})}/>
-            <button onClick={() => this.setState({resultado: parseInt(this.state.numero1) + parseInt(this.state.numero2)})}>Somar </button>
-
-            O resultado é: {this.state.resultado}
-          </div>
+            <div>
+                <Login/>
+            </div>
         )
-      }
+
     }
+
+}
 
 export default App;
