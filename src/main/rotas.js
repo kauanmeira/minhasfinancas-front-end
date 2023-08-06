8ce5926c2b5489603660cd262c65543af0124d06
kauan.meira <kauangabriel.meira123@gmail.com>
2023-08-06 00:14:07 -0300

    Criação da View Home

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Login from "../views/login";
 import CadastroUsuario from "../views/cadastroUsuario";
+import Home from '../views/home'
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 
-function Rotas(){
+function Rotas() {
     return (
         <Router>
             <Routes>
-                <Route path ="/login" element={<Login />} />
+                <Route path="/login" element={<Login />} />
                 <Route path="/cadastro-usuarios" element={<CadastroUsuario />} />
+                <Route path="/home" element={<Home />} />
+
             </Routes>
         </Router>
     )
