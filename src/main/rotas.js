94eb66d6558ee39c6d1d8b9f7f6033ff52a132df
kauan.meira <kauangabriel.meira123@gmail.com>
2023-08-08 14:52:58 -0300

    Implementação da view Consulta Lançamentos

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "../views/login";
 import CadastroUsuario from "../views/cadastroUsuario";
+import ConsultaLancamentos from "../views/lancamentos/consulta-lancamentos";
 import Home from '../views/home'
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 
@@ -11,7 +12,7 @@ function Rotas() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/cadastro-usuarios" element={<CadastroUsuario />} />
                 <Route path="/home" element={<Home />} />
-
+                <Route path="/consulta-lancamentos" element={<ConsultaLancamentos />} />
             </Routes>
         </Router>
     )
---
+++ src/views/lancamentos/consulta-lancamentos.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { Form, useNavigate } from 'react-router-dom';
+import Card from "../../components/card";
+import FormGroup from "../../components/form-group";
+import selectMenu from "../../components/selectMenu";
+import SelectMenu from "../../components/selectMenu";
+import lancamentosTable from "./lancamentosTable";
+import LancamentosTable from "./lancamentosTable";
+
+class ConsultaLancamentos extends React.Component {
+
+
+    render() {
+        const meses = [
+            { label: 'Selecione...', value: '' },
+            { label: 'Janeiro', value: 1 },
+            { label: 'Fevereiro', value: 2 },
+            { label: 'Março', value: 3 },
+            { label: 'Abril', value: 4 },
+            { label: 'Maio', value: 5 },
+            { label: 'Junho', value: 6 },
+            { label: 'Julho', value: 7 },
+            { label: 'Agosto', value: 8 },
+            { label: 'Setembro', value: 9 },
+            { label: 'Outubro', value: 10 },
+            { label: 'Novembro', value: 11 },
+            { label: 'Dezembro', value: 12 }
+        ]
+
+        const tipos = [
+            { label: 'Selecione...', value: '' },
+            { label: 'Despesa', value: 'DESPESA' },
+            { label: 'Receita', value: 'RECEITA' }
+        ]
+
+        const lancamentos = [
+            { id: 1, descricao: 'Salário', valor: 5000.0, mes: 8, tipo: 'RECEITA', status: 'EFETIVADO' }
+        ]
+
+
+
+        return (
+            <Card title="Consulta Lançamentos">
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="bs-component">
+                            <FormGroup htmlFor="inputAno" label="Ano: *">
+                                <input type="text"
+                                    className="form-control"
+                                    id="inputAno"
+                                    aria-aria-describedby="emailHelp"
+                                    placeholder="Digite o Ano" />
+                            </FormGroup>
+                            <FormGroup htmlFor="inputMes" label="Mês: ">
+                                <SelectMenu id="inputMes" className="form-control" lista={meses} />
+                            </FormGroup>
+                            <FormGroup htmlFor="inputTipo" label="Tipo: ">
+                                <SelectMenu id="inputTipo" className="form-control" lista={tipos} />
+                            </FormGroup>
+
+                            <button type="button" className="btn btn-success">Buscar</button>
+                            <button type="button" className="btn btn-danger">Cadastrar</button>
+
+
+                        </div>
+                    </div>
+                </div>
+                <br />
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="bs-component">
+                            <LancamentosTable lancamentos={lancamentos} />
+                        </div>
+                    </div>
+                </div>
+            </Card>
+        )
+    }
+
+}
+
+export default ConsultaLancamentos
